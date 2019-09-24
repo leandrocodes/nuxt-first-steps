d58a3368efd6b33da58b18b2b6e7cc9976c8d3d6
Leandro <leaxviana140@gmail.com>
2019-09-24 09:23:50 -0400

    joke id

--- pages/jokes/_id/index.vue
+++ pages/jokes/_id/index.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
-        Joke {{ $route.params.id }}
+        <h3>{{ joke }}</h3>
+        <small>Joke ID: {{ $route.params.id }}</small>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+    data() {
+        return {
+            joke: {}
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.get(
+                `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+                config
+            )
+            console.log(res.data.joke)
+            this.joke = res.data.joke
+            //console.log(joke)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 </script>
 
 <style>
-
 </style>
--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,48 +1,51 @@
 <template>
-  <div>
-    <Joke v-for="i in jokes" :key="i.id" :id="i.id" :joke="i.joke"/>
-  </div>
+    <div>
+        <Joke v-for="i in jokes" :key="i.id" :id="i.id" :joke="i.joke" />
+    </div>
 </template>
 
 <script>
 import axios from 'axios'
 import Joke from '../../components/Joke'
 export default {
-  components:{
-    Joke
-  },
-  data() {
-    return {
-      jokes: []
-    }
-  },
-  async created() {
-    const config = {
-      headers: {
-        Accept: 'application/json'
-      }
-    }
-
-    try {
-      const res = await axios.get('https://icanhazdadjoke.com/search', config)
-      //console.log(res.data.results)
-      this.jokes = res.data.results
-    } catch (error) {
-      console.log(error)
-    }
-  },
-  head() {
-    return {
-      title: 'Jokes List - Dad Jokes',
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'best place for the best jokes'
+    components: {
+        Joke
+    },
+    data() {
+        return {
+            jokes: []
         }
-      ]
+    },
+    async created() {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.get(
+                'https://icanhazdadjoke.com/search',
+                config
+            )
+            //console.log(res.data.results)
+            this.jokes = res.data.results
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    head() {
+        return {
+            title: 'Jokes List - Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'best place for the best jokes'
+                }
+            ]
+        }
     }
-  }
 }
 </script>
 
