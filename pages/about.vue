21d63689ec82be8a8a45f451f55c72ef6866854e
Leandro <leaxviana140@gmail.com>
2019-09-24 08:21:27 -0400

    finish styling

--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,24 @@
 <template>
-    <div>
-        <h1>About DadJokes</h1>
-        <p>This is an app that display corny dadjokes</p>
-    </div>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that display corny dadjokes</p>
+  </div>
 </template>
 
 <script>
 export default {
-    head() {
-        return {
-            title: 'About Us - Dad Jokes',
-            meta: [
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'best place for the best jokes'
-                }
-            ]
+  head() {
+    return {
+      title: 'About Us - Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'best place for the best jokes'
         }
+      ]
     }
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,23 @@
 <template>
-    <div>
-        <h2>Welcome to the best Corny DadJokes App!</h2>
-    </div>
+  <div>
+    <h2>Welcome to the best Corny DadJokes App!</h2>
+  </div>
 </template>
 
 <script>
 export default {
-    head() {
-        return {
-            title: 'Home - Dad Jokes',
-            meta: [
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'best place for the best jokes'
-                }
-            ]
+  head() {
+    return {
+      title: 'Home - Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'best place for the best jokes'
         }
+      ]
     }
+  }
 }
 </script>
 
